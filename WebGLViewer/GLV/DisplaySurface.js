a79e7e3c0c6abd0d1ae365dcee13e88a194791c1
Cezary Bella <cezary.bella@gmail.com>
2018-11-03 23:54:52 +0100

    ViewMatrix for Plane Computation!

--- WebGLViewer/GLV/DisplaySurface.js
+++ WebGLViewer/GLV/DisplaySurface.js
@@ -13,9 +13,14 @@ DisplaySurface.prototype.viewingMatrix = function(eye){
     mat.loadIdentity();
 	
 	// TBC: this example does NOT create the correct viewing matrix
-	var eye = new Vec3(0.0, 50.0, 100.0);
-	var vrp = new Vec3(0.0, 0.0, 0.0);
 	var up = new Vec3(0.0, 1.0, 0.0);
+	
+	var normal = Vec3.cross(this.u, this.v).normalize();
+
+	var value_t = (normal.x * this.origin.x - normal.x * eye.x + normal.y * this.origin.y - normal.y * eye.y + normal.z * this.origin.z - normal.z * eye.z) / (normal.x * normal.x + normal.y * normal.y + normal.z * normal.z);
+
+	var vrp = new Vec3(eye.x + value_t * normal.x, eye.y + value_t * normal.y, eye.z + value_t * normal.z);
+
 	mat.lookAt(eye, vrp, up);
 	
 	return mat;
